b6867f46346443cbd84caffbe33e31897dad1fc0
Kamil <kamil@ethworks.io>
2019-12-20 15:51:09 +0100

    Change dashboard

--- gnt2-migration-ui/src/ui/Dashboard.tsx
+++ gnt2-migration-ui/src/ui/Dashboard.tsx
@@ -1,30 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {Login} from './Login';
 import {Account} from './Account';
-import {useAsync} from './hooks/useAsync';
-import {useServices} from './useServices';
-import {Web3Provider} from 'ethers/providers';
 
-export const Dashboard = () => {
-  const [provider, setProvider] = useState(undefined as Web3Provider | undefined);
-  const {connectionService} = useServices();
-
-  useAsync(async () => {
-    const provider = await connectionService.connect();
-    setProvider(provider);
-  }, []);
-
-  const Root = provider ? Account : Login;
-
-  return (
+export const Dashboard = () =>
+  (
     <div>
       <BrowserRouter>
         <Switch>
           <Route path='/account' component={Account}/>
-          <Route exact path='/' component={Root}/>
+          <Route exact path='/' component={Login}/>
         </Switch>
       </BrowserRouter>
     </div>
   );
-};
