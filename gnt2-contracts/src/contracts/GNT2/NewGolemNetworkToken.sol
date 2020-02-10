849c4b5c17559f8856f7578266eb183d3fd1d6e2
Krzysztof Jelski <krzysztof@ethworks.io>
2020-02-10 16:05:57 +0100

    WIP on permit

--- gnt2-contracts/src/contracts/GNT2/NewGolemNetworkToken.sol
+++ gnt2-contracts/src/contracts/GNT2/NewGolemNetworkToken.sol
@@ -7,11 +7,24 @@ contract NewGolemNetworkToken is ERC20Mintable {
     string public name = "New Golem Network Token";
     string public symbol = "NGNT";
     uint8 public decimals = 18;
+    string  public constant version  = "1";
+    mapping(address => uint) public nonces;
 
+    // --- EIP712 niceties ---
+    bytes32 public DOMAIN_SEPARATOR;
+    // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
+    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
 
-    constructor(address _migrationAgent) public {
+    constructor(address _migrationAgent, uint256 _chainId) public {
         addMinter(_migrationAgent);
         renounceMinter();
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                _chainId,
+                address(this)
+            ));
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
@@ -20,5 +33,28 @@ contract NewGolemNetworkToken is ERC20Mintable {
             _approve(sender, msg.sender, allowance(sender, msg.sender).sub(amount));
         }
         return true;
+    }
+
+    // --- Approve by signature ---
+    function permit(address holder, address spender, uint256 nonce, uint256 expiry,
+        bool allowed, uint8 v, bytes32 r, bytes32 s) external {
+        bytes32 digest =
+        keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH,
+                holder,
+                spender,
+                nonce,
+                expiry,
+                allowed))
+            ));
+
+        require(holder != address(0), "Dai/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Dai/invalid-permit");
+        require(expiry == 0 || now <= expiry, "Dai/permit-expired");
+        require(nonce == nonces[holder]++, "Dai/invalid-nonce");
+        uint wad = allowed ? uint(- 1) : 0;
+        _approve(holder, spender, wad);
     }
 }
--- gnt2-contracts/test/contracts/NewGolemNetworkToken.ts
+++ gnt2-contracts/test/contracts/NewGolemNetworkToken.ts
@@ -1,17 +1,66 @@
-import {expect} from 'chai';
-import {createMockProvider, deployContract, getWallets} from 'ethereum-waffle';
-import NewGolemNetworkToken from '../../build/NewGolemNetworkToken.json';
-import {ContractJSON} from 'ethereum-waffle/dist/esm/ContractJSON';
+import chai, {expect} from 'chai';
+import {createMockProvider, getWallets, solidity} from 'ethereum-waffle';
+import {NewGolemNetworkTokenFactory} from '../..';
+import {
+  BigNumber, BigNumberish,
+  keccak256,
+  parseEther,
+  SigningKey,
+  solidityKeccak256
+} from 'ethers/utils';
+import {ethers} from 'ethers';
+import {NewGolemNetworkToken} from '../../build/contract-types/NewGolemNetworkToken';
+chai.use(solidity)
 
 describe('New Golem Network Token', () => {
   const provider = createMockProvider();
-  const [sender] = getWallets(provider);
-  let token;
+  const [deployWallet, minter, holder, spender, thirdWallet] = getWallets(provider);
 
   it('is deployed properly', async () => {
-    token = await deployContract(sender, NewGolemNetworkToken as ContractJSON);
+    const token = await new NewGolemNetworkTokenFactory(deployWallet).deploy(minter.address, 4);
+
     expect(await token.name()).to.eq('New Golem Network Token');
     expect(await token.symbol()).to.eq('NGNT');
     expect(await token.decimals()).to.eq(18);
   });
+
+  async function mint(token: NewGolemNetworkToken, account: string, amount: BigNumberish) {
+    await NewGolemNetworkTokenFactory.connect(token.address, provider.getSigner(minter.address)).mint(account, amount);
+  }
+
+  it('can approve by signature', async () => {
+    const token = await new NewGolemNetworkTokenFactory(deployWallet).deploy(minter.address, 4);
+    const signature = await signPermitDigest(token, holder.address, spender.address, 0, 0, true);
+    await token.permit(holder.address, spender.address, 0, 0, true, signature.v!, signature.r, signature.s, {gasLimit: 1000000});
+  });
+
+  it('can spent approved token', async () => {
+    const token = await new NewGolemNetworkTokenFactory(deployWallet).deploy(minter.address, 4);
+    await mint(token, holder.address, parseEther('100'))
+    const signature = await signPermitDigest(token, holder.address, spender.address, 0, 0, true);
+    await token.permit(holder.address, spender.address, 0, 0, true, signature.v!, signature.r, signature.s, {gasLimit: 1000000});
+    const tokenAsSpender = NewGolemNetworkTokenFactory.connect(token.address, spender);
+    await tokenAsSpender.transferFrom(holder.address, thirdWallet.address, parseEther('100'), {gasLimit: 2000000})
+    await expect(() => tokenAsSpender.transferFrom(holder.address, thirdWallet.address, parseEther('100')))
+      .to.changeBalances([holder, thirdWallet], [parseEther('-100'), parseEther('100')]);
+
+  });
+
+  async function signPermitDigest(
+    token: NewGolemNetworkToken,
+    holderAddress: string,
+    spenderAddress: string,
+    nonce: number,
+    expiry: number,
+    allowed: boolean
+  ) {
+    const inner = keccak256(ethers.utils.defaultAbiCoder.encode(
+      ['bytes32', 'address', 'address', 'uint256', 'uint256', 'bool'],
+      [await token.PERMIT_TYPEHASH(), holderAddress, spenderAddress, nonce, expiry, allowed]));
+    const message = solidityKeccak256(
+      ['bytes2', 'bytes32', 'bytes32'],
+      ['0x1901', await token.DOMAIN_SEPARATOR(), inner]
+    );
+    return new SigningKey(holder.privateKey).signDigest(message);
+  }
 });
