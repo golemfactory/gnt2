b6b626beb2b7c9df5dee955299f7d55495d8be05
alexcherman <sashacherman@gmail.com>
2020-03-12 17:31:48 +0100

    Add dashboard layout

--- gnt2-migration-ui/src/index.tsx
+++ gnt2-migration-ui/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import './styles/index.sass';
 import './types/index.ts';
 import App from './ui/App';
 import {createServices} from './services';
---
+++ gnt2-migration-ui/src/ui/commons/Text/Text.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Text = styled.p`
+  font-size: 18px;
+  line-height: 30px;
+  color: #1722A2;
+  opacity: 0.6;
+`;
