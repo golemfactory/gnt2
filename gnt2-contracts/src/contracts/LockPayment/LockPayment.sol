bcf9c07a78f75724ed44a32bea26f53a0846fe56
Kamil Koczurek <koczurekk@gmail.com>
2024-05-15 16:07:06 +0200

    clean up IERC20

--- gnt2-contracts/src/contracts/LockPayment/LockPayment.sol
+++ gnt2-contracts/src/contracts/LockPayment/LockPayment.sol
@@ -5,9 +5,7 @@ pragma solidity ^0.8.13;
  * Out of the IERC20 interface, only the transfer and transferFrom functions are used.
  */
 interface IERC20 {
-    //function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
-
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
