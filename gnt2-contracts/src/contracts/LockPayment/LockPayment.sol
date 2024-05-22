da03e5c730d42d8db2fa873c873e203c13ad284a
scx1332 <sieciech.czajka@golem.network>
2024-05-22 11:11:17 +0200

    New version of deposit contract

--- gnt2-contracts/src/contracts/LockPayment/LockPayment.sol
+++ gnt2-contracts/src/contracts/LockPayment/LockPayment.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.13;
  * Out of the IERC20 interface, only the transfer and transferFrom functions are used.
  */
 interface IERC20 {
-    //function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
 
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
@@ -17,24 +16,16 @@ interface IERC20 {
   * - Funder - the address that deposits the funds
   */
 
-    struct Deposit {
-        address spender; //address that can spend the funds provided by customer
-        uint128 amount; //remaining funds locked
-        uint128 feeAmount; //fee amount locked for spender
-        uint64 validTo; //after this timestamp funds can be returned to customer
-    }
-
+interface ILockPayment {
     struct DepositView {
         uint256 id;     //unique id
         uint64 nonce;  //nonce unique for each funder
         address funder; //address that can spend the funds provided by customer
         address spender; //address that can spend the funds provided by customer
         uint128 amount; //remaining funds locked
-        uint128 feeAmount; //fee amount locked for spender
         uint64 validTo; //after this timestamp funds can be returned to customer
     }
 
-interface ILockPayment {
     function closeDeposit(uint256 id) external;
 
     function terminateDeposit(uint64 nonce) external;
@@ -46,8 +37,6 @@ interface ILockPayment {
     function depositSingleTransferAndClose(uint256 id, address addr, uint128 amount) external;
 
     function depositTransferAndClose(uint256 id, bytes32[] calldata payments) external;
-
-    function getMyDeposit(uint64 nonce) external view returns (DepositView memory);
 
     function getDeposit(uint256 id) external view returns (DepositView memory);
 
@@ -63,12 +52,19 @@ interface ILockPayment {
 contract LockPayment is ILockPayment {
     IERC20 public GLM;
 
-    uint64 immutable public CONTRACT_VERSION = 0x1;
+    struct Deposit {
+        address spender; //address that can spend the funds provided by customer
+        uint128 amount; //remaining funds locked (assuming max value 1 billion GLMs <=~ 2**90 gwei)
+        uint128 feeAmount; //fee amount locked for spender
+        uint64 validTo; //after this timestamp funds can be returned to customer
+    }
+
+    uint64 immutable public CONTRACT_VERSION = 0x2;
     uint64 immutable public CONTRACT_ID = 0x167583000; //6028800000
     // CONTRACT_ID_AND_VERSION = CONTRACT_ID ^ CONTRACT_VERSION
     // CONTRACT_ID_AND_VERSION = CONTRACT_ID | CONTRACT_VERSION
     // CONTRACT_ID_AND_VERSION = CONTRACT_ID + CONTRACT_VERSION
-    uint64 immutable public CONTRACT_ID_AND_VERSION = 0x167583001; //6028800001
+    uint64 immutable public CONTRACT_ID_AND_VERSION = 0x167583002; //6028800002
 
     event DepositCreated(uint256 id, address spender);
     event DepositExtended(uint256 id, address spender);
@@ -84,6 +80,7 @@ contract LockPayment is ILockPayment {
         require(CONTRACT_ID_AND_VERSION == CONTRACT_ID | CONTRACT_VERSION);
         //id has special property that CONTRACT_ID | CONTRACT_VERSION == CONTRACT_ID + CONTRACT_VERSION
         require(CONTRACT_ID | CONTRACT_VERSION == CONTRACT_ID + CONTRACT_VERSION);
+        require(CONTRACT_ID ^ CONTRACT_VERSION == CONTRACT_ID + CONTRACT_VERSION);
         GLM = _GLM;
     }
 
@@ -103,20 +100,15 @@ contract LockPayment is ILockPayment {
         return address(uint160(id >> 96));
     }
 
-    function getMyDeposit(uint64 nonce) public view returns (DepositView memory) {
-        Deposit memory deposit = deposits[idFromNonce(nonce)];
-        return DepositView(idFromNonce(nonce), nonce, funderFromId(idFromNonce(nonce)), deposit.spender, deposit.amount, deposit.feeAmount, deposit.validTo);
-    }
-
     function getDeposit(uint256 id) public view returns (DepositView memory) {
         Deposit memory deposit = deposits[id];
-        return DepositView(id, nonceFromId(id), funderFromId(id), deposit.spender, deposit.amount, deposit.feeAmount, deposit.validTo);
+        return DepositView(id, nonceFromId(id), funderFromId(id), deposit.spender, deposit.amount, deposit.validTo);
     }
 
     function getDepositByNonce(uint64 nonce, address funder) public view returns (DepositView memory) {
         uint256 id = idFromNonceAndFunder(nonce, funder);
         Deposit memory deposit = deposits[id];
-        return DepositView(id, nonceFromId(id), funderFromId(id), deposit.spender, deposit.amount, deposit.feeAmount, deposit.validTo);
+        return DepositView(id, nonceFromId(id), funderFromId(id), deposit.spender, deposit.amount, deposit.validTo);
     }
 
     // createDeposit - Customer locks funds for usage by spender
@@ -125,18 +117,15 @@ contract LockPayment is ILockPayment {
     // spender - the address that is allowed to spend the funds regardless of time
     // amount - amount of GLM tokens to lock
     // flatFeeAmount - amount of GLM tokens given to spender (non-refundable). Fee is claimed by spender when called payoutSingle or payoutMultiple first time.
-    // percentFee - percent fee as percent of amount (given in parts/per million), so 1000 gives 0.1 %.
-    //              if given negative it is deducted from flatFeeAmount
-    //              IT IS NOT IMPLEMENTED IN THIS IMPLEMENTATION
-    // blockNo - block number until which funds are guaranteed to be locked for spender.
-    //           Spender still can use the funds after this block,
-    //           but customer can request the funds to be returned clearing deposit after (or equal to) this block number.
-    function createDeposit(uint64 nonce, address spender, uint128 amount, uint128 flatFeeAmount, int64 percentFee, uint64 validToTimestamp) public returns (uint256) {
+    // validToTimestamp - time until which funds are guaranteed to be locked for spender.
+    //           Spender still can use the funds after this timestamp,
+    //           but customer can request the funds to be returned clearing deposit after (or equal to) this timestamp.
+    function createDeposit(uint64 nonce, address spender, uint128 amount, uint128 flatFeeAmount, uint64 validToTimestamp) public returns (uint256) {
         //check if id is not used
         uint256 id = idFromNonce(nonce);
-        require(deposits[id].amount == 0, "deposits[id].amount == 0");
+        //this checks if deposit is not already created with this id
+        require(deposits[id].spender == address(0), "deposits[id].spender == address(0)");
         require(amount > 0, "amount > 0");
-        require(percentFee == 0, "percentFee == 0 for this contract");
         require(spender != address(0), "spender cannot be null address");
         require(msg.sender != spender, "spender cannot be funder");
         require(GLM.transferFrom(msg.sender, address(this), amount + flatFeeAmount), "transferFrom failed");
@@ -170,6 +159,8 @@ contract LockPayment is ILockPayment {
         }
         deposits[id].amount = 0;
         deposits[id].feeAmount = 0;
+        //leave this in deposit to prevent recreating deposit with the same id
+        deposits[id].spender = 0x0000000000000000000000000000000000000Bad;
         emit DepositClosed(id, deposit.spender);
     }
 
@@ -184,6 +175,8 @@ contract LockPayment is ILockPayment {
         require(GLM.transfer(msg.sender, deposit.amount + deposit.feeAmount), "transfer failed");
         deposits[id].amount = 0;
         deposits[id].feeAmount = 0;
+        //leave this in deposit to prevent recreating deposit with the same id
+        deposits[id].spender = 0x0000000000000000000000000000000000000Bad;
         emit DepositTerminated(id, deposit.spender);
     }
 
@@ -191,7 +184,7 @@ contract LockPayment is ILockPayment {
         Deposit memory deposit = deposits[id];
         require(msg.sender == deposit.spender, "msg.sender == deposit.spender");
         require(addr != deposit.spender, "cannot transfer to spender");
-        require(GLM.transfer(addr, amount), "transferFrom failed");
+        require(GLM.transfer(addr, amount), "GLM transfer failed");
         require(deposit.amount >= amount, "deposit.amount >= amount");
         deposit.amount -= amount;
         deposits[id].amount = deposit.amount;
@@ -209,7 +202,7 @@ contract LockPayment is ILockPayment {
             address addr = address(bytes20(payment));
             uint128 amount = uint128(uint256(payment) % 2 ** 96);
             require(addr != deposit.spender, "cannot transfer to spender");
-            require(GLM.transfer(addr, amount), "transferFrom failed");
+            require(GLM.transfer(addr, amount), "GLM transfer failed");
             require(deposit.amount >= amount, "deposit.amount >= amount");
             deposit.amount -= amount;
         }
@@ -227,17 +220,20 @@ contract LockPayment is ILockPayment {
         closeDeposit(id);
     }
 
-    function validateDeposit(uint256 id, address spender, uint128 amount, uint128 flatFeeAmount, int64 percentFee, uint64 validToTimestamp) public view returns (bool) {
+    //validateDeposit - validate extra fields not covered by common validation
+    function validateDeposit(uint256 id, uint128 flatFeeAmount) public view returns (string memory) {
         Deposit memory deposit = deposits[id];
-        require(spender == deposit.spender, "msg.sender == deposit.spender");
-        require(amount == deposit.amount, "amount == deposit.amount");
-        require(flatFeeAmount == deposit.feeAmount, "flatFeeAmount == deposit.feeAmount");
-        require(percentFee == 0, "percentFee == 0 for this contract");
-        require(validToTimestamp < deposit.validTo, "validToTimestamp < deposit.validTo");
-        return true;
+        if (deposit.spender == address(0)) {
+            return "failed due to wrong deposit id";
+        }
+        if (flatFeeAmount != deposit.feeAmount) {
+            return "failed due to flatFeeAmount mismatch";
+        }
+        return "valid";
     }
 
     function getValidateDepositSignature() public pure returns (string memory) {
-        return "validateDeposit(uint256 id, address spender, uint128 amount, uint128 flatFeeAmount, int64 percentFee, uint64 validToTimestamp) public view returns (bool)";
+        // example implementation
+        return '[{"type": "uint256", "name": "id"}, {"type": "uint128", "name": "flatFeeAmount"}]';
     }
 }
