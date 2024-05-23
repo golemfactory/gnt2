33269c423d248d271f1830720620feceb0d05c63
scx1332 <sieciech.czajka@golem.network>
2024-05-23 12:36:41 +0200

    polishing contract

--- gnt2-contracts/src/contracts/WrapperCall/WrapperCall.sol
+++ gnt2-contracts/src/contracts/WrapperCall/WrapperCall.sol
@@ -3,31 +3,43 @@ pragma solidity ^0.8.13;
 
 contract WrapperCall {
     struct CallWithDetailsResult {
-        uint256 callerBalance;
+        uint256 chainId;
         uint256 blockNumber;
         uint256 blockTimestamp;
+        uint256 callerBalance;
         bytes callResult;
     }
 
     // Call an external contract with the provided call data and return the result along with useful details
     function callWithDetails(address externalContractAddress, bytes calldata callData) external returns (CallWithDetailsResult memory) {
-        (bool success, bytes memory result) = externalContractAddress.call(
-            callData
-        );
-        // Check if the call was successful
-        require(success, "External call failed");
+        bytes memory result;
+        if (externalContractAddress != address(0)) {
+            (bool success, bytes memory res) = externalContractAddress.call(callData);
+            // Check if the call was successful
+            require(success, "External call failed");
+            result = res;
+        } else {
+            result = "";
+        }
+
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
 
         return CallWithDetailsResult(
-            address(this).balance,
+            chainId,
             block.number,
             block.timestamp,
+            msg.sender.balance,
             result);
     }
 
     struct MultiCallWithDetailsResult {
-        uint256 callerBalance;
+        uint256 chainId;
         uint256 blockNumber;
         uint256 blockTimestamp;
+        uint256 callerBalance;
         bytes[] callResults;
     }
 
@@ -38,18 +50,34 @@ contract WrapperCall {
 
         bytes[] memory results = new bytes[](externalContractAddresses.length);
         for (uint256 i = 0; i < externalContractAddresses.length; i++) {
-            (bool success, bytes memory data) = externalContractAddresses[i].call(
-                callDataArray[i]
-            );
+            (bool success, bytes memory data) = externalContractAddresses[i].call(callDataArray[i]);
             // Check if the call was successful
             require(success, "External call failed");
             results[i] = data;
         }
 
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+
         return MultiCallWithDetailsResult(
-            address(this).balance,
+            chainId,
             block.number,
             block.timestamp,
+            msg.sender.balance,
             results);
+    }
+
+    // Standard multicall for chaining transactions
+    function multiCall(address[] calldata externalContractAddresses, bytes[] calldata callDataArray) external {
+        require(externalContractAddresses.length == callDataArray.length, "Arrays must be of equal length");
+        require(externalContractAddresses.length > 0, "Arrays must not be empty");
+
+        for (uint256 i = 0; i < externalContractAddresses.length; i++) {
+            (bool success,) = externalContractAddresses[i].call(callDataArray[i]);
+            // Check if the call was successful
+            require(success, "External call failed");
+        }
     }
 }
