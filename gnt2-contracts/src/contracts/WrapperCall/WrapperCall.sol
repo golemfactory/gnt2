ffaf3c9830a3e40bd5c05a91f7ed58b71ee3b432
scx1332 <sieciech.czajka@golem.network>
2024-05-23 11:28:35 +0200

    added balance to details and simplified the contract

--- gnt2-contracts/src/contracts/WrapperCall/WrapperCall.sol
+++ gnt2-contracts/src/contracts/WrapperCall/WrapperCall.sol
@@ -1,69 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-struct BlockDetails {
-    uint256 number;
-    uint256 timestamp;
-}
-
-struct CallWithDetailsResult {
-    BlockDetails details;
-    bytes result;
-}
-
-struct MultiCallWithDetailsResult {
-    BlockDetails details;
-    bytes[] results;
-}
-
 contract WrapperCall {
-    constructor() {
+    struct CallWithDetailsResult {
+        uint256 callerBalance;
+        uint256 blockNumber;
+        uint256 blockTimestamp;
+        bytes callResult;
     }
 
-    function detailsOnly() external view returns (BlockDetails memory) {
-        BlockDetails memory details = BlockDetails({
-            number: block.number,
-            timestamp: block.timestamp
-        });
-
-        return details;
-    }
-
+    // Call an external contract with the provided call data and return the result along with useful details
     function callWithDetails(address externalContractAddress, bytes calldata callData) external returns (CallWithDetailsResult memory) {
-        (bool success, bytes memory data) = externalContractAddress.call(
+        (bool success, bytes memory result) = externalContractAddress.call(
             callData
         );
-
-        BlockDetails memory details = BlockDetails({
-            number: block.number,
-            timestamp: block.timestamp
-        });
-
         // Check if the call was successful
         require(success, "External call failed");
 
-        return CallWithDetailsResult(details, data);
+        return CallWithDetailsResult(
+            address(this).balance,
+            block.number,
+            block.timestamp,
+            result);
     }
 
-    function multiCallWithDetails(address[] calldata externalContractAddresses, bytes[] calldata callDatas) external returns (MultiCallWithDetailsResult memory) {
-        require(externalContractAddresses.length == callDatas.length, "Arrays must be of equal length");
+    struct MultiCallWithDetailsResult {
+        uint256 callerBalance;
+        uint256 blockNumber;
+        uint256 blockTimestamp;
+        bytes[] callResults;
+    }
+
+    // Call an external contracts with the provided calls data and return the result along with useful details
+    function multiCallWithDetails(address[] calldata externalContractAddresses, bytes[] calldata callDataArray) external returns (MultiCallWithDetailsResult memory) {
+        require(externalContractAddresses.length == callDataArray.length, "Arrays must be of equal length");
         require(externalContractAddresses.length > 0, "Arrays must not be empty");
 
         bytes[] memory results = new bytes[](externalContractAddresses.length);
         for (uint256 i = 0; i < externalContractAddresses.length; i++) {
             (bool success, bytes memory data) = externalContractAddresses[i].call(
-                callDatas[i]
+                callDataArray[i]
             );
             // Check if the call was successful
             require(success, "External call failed");
             results[i] = data;
         }
 
-        BlockDetails memory details = BlockDetails({
-            number: block.number,
-            timestamp: block.timestamp
-        });
-
-        return MultiCallWithDetailsResult(details, results);
+        return MultiCallWithDetailsResult(
+            address(this).balance,
+            block.number,
+            block.timestamp,
+            results);
     }
 }
